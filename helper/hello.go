10cb38f3b07a23bef372484c4982ee3c8cc3451e
RageNeko26 <rneko2006@gmail.com>
2023-06-13 10:38:09 +0700

    feat: Other testing using testify

--- helper/hello.go
+++ helper/hello.go
@@ -1,5 +1,5 @@
 package helper
 
 func Hello(name string) string {
-	return "Hi " + name
+	return "Hello " + name
 }
--- helper/hello_test.go
+++ helper/hello_test.go
@@ -7,8 +7,24 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestSub(t *testing.T) {
+
+}
+
+func TestMain(m *testing.M) {
+	// Before unit test
+	fmt.Println("Before unit test...")
+	m.Run()
+	// After unit test
+	fmt.Println("After unit test...")
+}
+
 func TestAssertion(t *testing.T) {
 	result := Hello("RageNeko26")
 	assert.Equal(t, "Hello RageNeko26", result, "Result must be 'Hello RageNeko26' ")
-	fmt.Println("Testing...")
+}
+
+func TestOther(t *testing.T) {
+	var result = Hello("Budi")
+	assert.Equal(t, "Hello Budi", result, "Result must be same as expected 'Hello Budi!' ")
 }
